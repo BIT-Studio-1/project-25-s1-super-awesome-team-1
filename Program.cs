9f922df6e0f58269fb19a72eb0212d69fd2a2a52
melanie-01 <ostem1@student.op.ac.nz>
2025-05-22 11:52:31 +1200

    added dialgoue

--- Program.cs
+++ Program.cs
@@ -167,7 +167,6 @@ namespace Studio1Project
                         showMap();
                         break;
                     case "sleep":
-                        //include energy increase
                         Console.WriteLine("You lie back on the cold stone floor and close your eyes. Despite the discomfort, you manage to rest.");
                         Thread.Sleep(2000);
                         Console.WriteLine("Sleeping helped you regain some energy!!");
@@ -834,7 +833,7 @@ namespace Studio1Project
         static void showCommands()
         {
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("Type one of the following commands or select a room: \nshow inventory \nshow energy \nsearch");
+            Console.WriteLine("Type one of the following commands or select a room: \nshow inventory \nshow energy \nsearch \nmap \n");
             Console.WriteLine("***************************************************************************\n\n");
         }
 
